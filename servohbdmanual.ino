5dd71d7e7252e4f1184f2c3f747a7ef1064cb972
Bendeguz Horvath <bendeguz.horvath@gmail.com>
2018-03-19 08:15:22 +0000

    manually controlled servo

---
+++ servohbdmanual.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+
+Servo myServo;
+
+const int manual = 0;
+  const int potmeter = A0;
+  int val;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myServo.attach(9);
+   myServo.write(0);
+   
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (manual == 0) {
+  val = analogRead(potmeter);
+  val = map(val, 0,1023, 0, 180);
+  Serial.println(val);
+  myServo.write(val);
+  //delay(1000);
+}
+else{
+ myServo.write(0);
+ delay(2000);
+ myServo.write(180);
+  delay(2000);
+  for (int k = 0; k<=180; k=k+30){
+     myServo.write(k);
+ delay(2000);
+
+  }
+  
+  for (int k = 180; k>0; k=k-30){
+     myServo.write(k);
+ delay(2000);
+  }}
+}
