faf47739c2deef5ef365fa3a6bc00fd6a702c41a
Bendeguz Horvath <bendeguz.horvath@gmail.com>
2018-04-18 08:33:08 +0000

    temperature measure with ds18b20

---
+++ teperature.ino
@@ -0,0 +1,34 @@
+
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 2 
+/********************************************************************/
+//
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+void setup(void) 
+{  
+ // start serial port 
+ Serial.begin(9600); 
+ Serial.println("Dallas Temperature IC Control Library Demo"); 
+
+ sensors.begin(); 
+} 
+void loop(void) 
+{ 
+ 
+/********************************************************************/
+ Serial.print(" Requesting temperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ Serial.println("DONE"); 
+/********************************************************************/
+ Serial.print("Temperature is: "); 
+ Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+  
+   delay(1000); 
+} 
